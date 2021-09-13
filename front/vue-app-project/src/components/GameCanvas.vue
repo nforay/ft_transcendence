575f9fe00ad8ebdcc6263a97950e6b10a9442de1
nforay <nforay@student.42.fr>
2021-09-13 17:18:58 +0200

    Added interactive Paddle

--- front/vue-app-project/src/components/GameCanvas.vue
+++ front/vue-app-project/src/components/GameCanvas.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="game-canvas">
-    <canvas id="canvas"></canvas>
+    <canvas id="canvas" tabindex="0"
+    @keydown.up="leftPaddle.upPressed = true"
+    @keydown.down="leftPaddle.downPressed = true"
+    @keyup.up="leftPaddle.upPressed = false"
+    @keyup.down="leftPaddle.downPressed = false"></canvas>
   </div>
 </template>
 
@@ -9,6 +13,20 @@ import Vue from 'vue'
 import Component from 'vue-class-component'
 import { SocketManager } from '../utils/SocketManager'
 
+export class Paddle {
+  x: number
+  y: number
+  width: number
+  height: number
+  upPressed = false
+  downPressed = false
+  constructor (x: number, y: number, width: number, height: number) {
+    this.x = x
+    this.y = y
+    this.width = width
+    this.height = height
+  }
+}
 @Component
 export default class GameCanvas extends Vue {
   canvas: HTMLCanvasElement | undefined
@@ -16,9 +34,35 @@ export default class GameCanvas extends Vue {
 
   socketManager = new SocketManager('http://localhost:4001')
 
+  leftPaddle = new Paddle(0, 0, 10, 100)
+  rightPaddle = new Paddle(40, 0, 10, 100)
   backgroundColor = '#000'
   paddleColor = '#fff'
   ballColor = '#fff'
+  paddleHeight = 80
+  pixel = 15
+
+  gameLoop () : void {
+    const dir = +this.leftPaddle.downPressed - (+this.leftPaddle.upPressed)
+    this.leftPaddle.y += dir * this.pixel
+    this.draw()
+    window.requestAnimationFrame(this.gameLoop)
+  }
+
+  draw () : void {
+    this.ctx.fillStyle = this.backgroundColor
+    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+
+    this.ctx.fillStyle = 'grey'
+    this.ctx.fillRect(0, 0, this.canvas.width, this.pixel) // top wall
+    this.ctx.fillRect(0, this.canvas.height - this.pixel, this.canvas.width, this.canvas.height) // bottom wall
+    for (let i = this.pixel; i < this.canvas.height - this.pixel; i += this.pixel * 2) {
+      this.ctx.fillRect(this.canvas.width / 2 - this.pixel / 2, i, this.pixel, this.pixel) // middle line
+    }
+    this.ctx.fillStyle = 'white'
+    this.ctx.fillRect(this.leftPaddle.x, this.leftPaddle.y, this.leftPaddle.width, this.leftPaddle.height) // left paddle
+    this.ctx.fillRect(this.rightPaddle.x, this.rightPaddle.y, this.rightPaddle.width, this.rightPaddle.height) // right paddle
+  }
 
   initCanvas () : void {
     this.canvas = document.getElementById('canvas') as HTMLCanvasElement
@@ -29,8 +73,10 @@ export default class GameCanvas extends Vue {
     this.canvas.width = reference * 0.8 // 80% of the reference
     this.canvas.height = reference * 0.8 // 1:1 aspect ratio
 
-    this.ctx.fillStyle = this.backgroundColor
-    this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height)
+    this.rightPaddle.x = this.canvas.width - this.rightPaddle.width
+    this.rightPaddle.y = this.canvas.height / 2 - this.rightPaddle.height / 2
+    this.draw()
+    window.requestAnimationFrame(this.gameLoop)
   }
 
   mounted () : void {
